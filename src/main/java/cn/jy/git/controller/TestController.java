521b57d9799be3c547abab027ab56e2cfeac89e5
lipanpan <lz_lipanpan@163.com>
2022-03-31 18:23:07 +0800

    这是dev分支第一次提交

--- src/main/java/cn/jy/git/controller/TestController.java
+++ src/main/java/cn/jy/git/controller/TestController.java
@@ -3,5 +3,6 @@ package cn.jy.git.controller;
 public class TestController {
     public static void main(String[] args){
         System.out.println("hello git");
+        System.out.println("hello git dev");
     }
 }
